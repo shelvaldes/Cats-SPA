4b383ff388d2b45c543798168178f335dc935ee0
Shel Valdés <creative@shelvaldes.com>
2021-02-17 23:10:47 -0600

    license

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+    const view = `
+    <div class="cats">
+        <article class="cat-item">
+            <a href="#/1/">
+                <img src="image" alt="name">
+                <h2>Author</h2>
+            </a>
+        </article>
+    </div>
+    
+    `
+    return view
+}
+
+export default Home
