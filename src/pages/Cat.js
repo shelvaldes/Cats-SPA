b2ed54d5e9017d59888939af56dc3a5d904a2abb
Shel Valdés <creative@shelvaldes.com>
2021-03-02 00:29:28 -0600

    Templates de gato y home

---
+++ src/pages/Cat.js
@@ -0,0 +1,16 @@
+const Cat = () => {
+    const view = `
+        <div class="cats-inner">
+            <article class="cats-card">
+                <img src="image" alt="name">
+                <h2>Name</h2>
+            </article>
+            <article class="Cats-card">
+                <h3>Desciption:</h3>
+                <h3>Color:</h3>
+            </article>
+        </div>
+    `
+    return view
+}
+export default Cat
