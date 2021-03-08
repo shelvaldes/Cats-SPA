403df394aaf5cbdc9a5d7815582ecfcf4c155ac7
Shel Valdés <creative@shelvaldes.com>
2021-03-08 16:39:12 -0600

    All messed up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("hello")
+import Header from './templates/Header'
+import Home from './pages/Home'
+import Cat from './pages/Cat'
+import Error404 from './pages/Error404'
+
+const routes = {
+    '/': Home,
+    '/:id': Cat,
+    '/contact': 'Contact',
+}
+
+const router = async () => {
+    const header = null || document.getElementById('header')
+    const content = null || document.getElementById('content')
+
+    header.innerHTML = await Header()
+}
