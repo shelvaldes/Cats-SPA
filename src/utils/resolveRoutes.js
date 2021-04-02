9b3141278b7820ab27ee5ec2f3c983caa9c3b3d3
Shel Valdés <creative@shelvaldes.com>
2021-04-02 02:47:59 -0600

    Bug fixed routes

--- src/utils/resolveRoutes.js
+++ src/utils/resolveRoutes.js
@@ -1,5 +1,5 @@
 const resolveRoutes = (route) => {
-    if(route.lenght <= 3) {
+    if(route.length <= 3) {
         let validRoute = route === '/' ? route : '/:id'
         return validRoute
     }
