f87cb0cac8c7a5711c852988fe046beede867261
Shel Valdés <creative@shelvaldes.com>
2021-04-02 23:52:35 -0600

    Cagadero api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,23 @@
-const Home = () => {
+import getData from "../utils/getData"
+
+import getData from '../utils/getData.js'
+
+const Home = async () => {
+    const cats = await getData()
+
     const view = `
     <div class="Cats">
+        ${cats.results.map(chat = => `
         <article class="Cat-item">
-            <a href="#/1/">
-                <img src="image" alt="name">
-                <h2>Author</h2>
-            </a>
-        </article>
+        <a href="#/1/">
+            <img src="image" alt="name">
+            <h2>Author</h2>
+        </a>
+    </article>
+        
+        `).join('')}
+
+        
     </div>
     
     `
